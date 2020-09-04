326c61b6ec6a662d180913fd707a8affaaaa98e8
Alex 53856 <alex53856@gchq.gov.uk>
2020-09-04 16:22:47 +0000

    unzipping and MRT functionality there

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
+//TODO
+//If there is no valid data when requesting the file then do not create the file
+//goroutine for decompression - mrtparse and bzip should be done in one goroutine
+//logging needs to be better
+//better handling of errors
+//too reliant on rib directory
+//if the date 0001-01-01 00:00:00 +0000 UTC then couldn't parse the page
 
 import (
 	"fmt"
@@ -7,37 +14,182 @@ import (
 	"os"
 	"github.com/gocolly/colly"
 	"strings"
+	"time"
+	"sync"
+	"compress/bzip2"
+	"io/ioutil"
+	"os/exec"
 
 
 )
 
 func main() {
+	archiveURL := "http://archive.routeviews.org/"
 	collectorUrl := "http://www.routeviews.org/routeviews/index.php/collectors/"
 	collectors := GetCollectors(collectorUrl)
 	fmt.Println("Finished Getting Collectors")
-}
-
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) error {
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
+	var wg sync.WaitGroup
+	for index, collectorName := range collectors {
+		wg.Add(1)
+		latestCollection := LatestCollection(collectorName, archiveURL)
+		fmt.Printf("%v Latest Collection %v\n", collectorName, latestCollection)
+		go getFile(&wg, collectorName, archiveURL, latestCollection)
+		if index > 4 {
+			break
+		}
 	}
-	defer resp.Body.Close()
+	wg.Wait()
+	fmt.Println("Done Collecting Files...")
+	fmt.Println("Unzipping Directory...")
+	unzipFiles("ribs/")
+	fmt.Println("Directory Unzipped")
+	fmt.Println("Init BGP Scanner")
+	collectBGPScanner("ribs/")
+	fmt.Println("Done.")
+}
+//todo add function to list directory and return a slice of files
 
-	// Create the file
-	out, err := os.Create(filepath)
+func collectBGPScanner(inputDirectory string) {
+	fmt.Println("Collecting BPG Scanner Tasks")
+	files, err := ioutil.ReadDir(inputDirectory)
 	if err != nil {
-		return err
+        fmt.Println(err)
+	}
+	var wg sync.WaitGroup
+    for _, f := range files {
+		wg.Add(1)
+		go spawnBGPScanner(&wg, fmt.Sprintf("%v%v", inputDirectory, f.Name()), fmt.Sprintf("parsed_ribs/%v", f.Name()))
+	}
+	wg.Wait()
+}
+func spawnBGPScanner(wg *sync.WaitGroup, inputFilepath string, outputFilepath string){
+	defer wg.Done()
+	fmt.Println("Spawned a BGP Scanner...")
+	cmd := exec.Command("/home/ubuntu/bgpscanner/build/bgpscanner", inputFilepath)
+	cmd.Stderr = os.Stderr
+	outfile, err := os.Create(outputFilepath)
+    if err != nil {
+        panic(err)
+    }
+    defer outfile.Close()
+    cmd.Stdout = outfile
+
+    err = cmd.Start(); if err != nil {
+        panic(err)
+    }
+    cmd.Wait()
+}
+func unzipFile(wg *sync.WaitGroup, outDir string, inputFilepath string) {
+	//TODO Remove the old bzip file when done
+	defer wg.Done()
+	inputFile, err := os.OpenFile(inputFilepath, 0, 0)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(inputFilepath)
+	inputReader := bzip2.NewReader(inputFile)
+	out, err := os.Create(strings.Replace(inputFilepath, ".bz2", "", -1))
+	if err != nil {
+		fmt.Println(err)
 	}
 	defer out.Close()
+	// Write the body to file
+	_, err = io.Copy(out, inputReader)
+	if err != nil {
+		fmt.Println(err)
+	}
+	os.Remove(inputFilepath)
+}
+func unzipFiles(directory string){
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+        fmt.Println(err)
+	}
+	var wg sync.WaitGroup
+    for _, f := range files {
+		wg.Add(1)
+		go unzipFile(&wg, "ribs/", fmt.Sprintf("%v%v", directory, f.Name()))
+	}
+	wg.Wait()
+}
+func mrtParseFile(){
 
+}
+func getFile(wg *sync.WaitGroup, collectorName string, url string, collectionTime time.Time) {
+	// Get the data
+	defer wg.Done()
+	fmt.Printf("Collecting %v...\n", collectorName)
+	collectionMonth := collectionTime.Format("2006.01")
+	fileName := fmt.Sprintf("rib.%v.bz2", collectionTime.Format("20060102.1504"))	
+	fullURL := 	fmt.Sprintf("%v%v/bgpdata/%v/RIBS/%v",
+		url, 
+		collectorName, 
+		collectionMonth,
+		fileName,
+	)
+	resp, err := http.Get(fullURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	// Create the file
+	filepath := fmt.Sprintf("ribs/%v-%v", collectorName, fileName)
+	out, err := os.Create(filepath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer out.Close()
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
-	return err
+	if err != nil {
+		fmt.Println(err)
+	}
 }
+func LatestCollection(collectorName string, url string) time.Time {
+	//example url http://archive.routeviews.org/route-views.amsix/bgpdata/2020.09/RIBS/rib.20200901.0000.bz2
+	layout := "20060102.1504"
+	latestCollectionMonth := LatestMonth(collectorName, url)
+	collectorDates := []time.Time{}
+	url = fmt.Sprintf("%v%v/bgpdata/%v/RIBS/", url, collectorName, fmt.Sprintf(latestCollectionMonth.Format("2006.01")))
+	c := colly.NewCollector()
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Text, "rib.") {
+			element := strings.Join(strings.Split(e.Text, ".")[1:3], ".")
+			colTime, err := time.Parse(layout, element)
+			if err == nil {
+				collectorDates = append(collectorDates, colTime)
+			}
+		}
+	})
+	c.Visit(url)
+	if len(collectorDates) == 0{
+		return time.Time{}
+	} else {
+		return collectorDates[len(collectorDates)-1]
+	}
+
+}
+func LatestMonth(collectorName string, url string) time.Time {
+	layout := "2006.01"
+	collectorDates := []time.Time{}
+	url = fmt.Sprintf("%v%v/bgpdata/", url, collectorName)
+	c := colly.NewCollector()
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		colTime, err := time.Parse(layout, strings.Trim(e.Text, "/"))
+		if err == nil {
+			collectorDates = append(collectorDates, colTime)
+		}
+	})
+	c.Visit(url)
+	if len(collectorDates) == 0{
+		return time.Time{}
+	} else {
+		return collectorDates[len(collectorDates)-1]
+	}
+}
+
 func GetCollectors(url string) []string {
 	collectors := []string{}
 	c := colly.NewCollector()
@@ -46,15 +198,20 @@ func GetCollectors(url string) []string {
 		e.ForEach("tr", func(_ int, row *colly.HTMLElement) {
 			//this will return the row in the table, then need to get more granular and get the elements in columns
 			row.ForEach("td", func(_ int, column *colly.HTMLElement) {
+				val := ""
 				if strings.Contains(column.Text, "routeviews"){
 					if strings.Contains(column.Text, "\n"){
 						//this is probably the oregon one so trim newline, split and get first
 						element := strings.Trim(column.Text, "\n")
-						val := strings.Split(element, "\n")[0]
-						collectors = append(collectors, val)
+						val = strings.Split(element, "\n")[0]
 					} else {
-						collectors = append(collectors, column.Text)
+						val = strings.Trim(column.Text, " ")
 					}
+					if val != "" {
+						val = strings.Replace(val, ".routeviews.org", "", -1)
+						collectors = append(collectors, val)
+					}
+
 				}
 			})
 
